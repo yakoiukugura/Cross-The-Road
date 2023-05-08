5d50027e590fad56a7567b4eaa694502ad82f93a
Yakoiukugura <56473303+yakoiukugura@users.noreply.github.com>
2023-05-08 23:51:29 +0300

    Add files via upload

---
+++ Cross The Road/Cross The Road/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿
+namespace Cross_The_Road
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menu = new System.Windows.Forms.Panel();
+            this.quit_button = new System.Windows.Forms.Button();
+            this.start_button = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.chicken = new System.Windows.Forms.PictureBox();
+            this.car2 = new System.Windows.Forms.PictureBox();
+            this.car1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Road2 = new System.Windows.Forms.PictureBox();
+            this.Road1 = new System.Windows.Forms.PictureBox();
+            this.menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chicken)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(205)))), ((int)(((byte)(50)))));
+            this.menu.BackgroundImage = global::Cross_The_Road.Properties.Resources.background;
+            this.menu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.menu.Controls.Add(this.quit_button);
+            this.menu.Controls.Add(this.start_button);
+            this.menu.Controls.Add(this.title);
+            this.menu.Location = new System.Drawing.Point(-66, -2);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(658, 611);
+            this.menu.TabIndex = 15;
+            // 
+            // quit_button
+            // 
+            this.quit_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quit_button.Location = new System.Drawing.Point(279, 355);
+            this.quit_button.Name = "quit_button";
+            this.quit_button.Size = new System.Drawing.Size(141, 61);
+            this.quit_button.TabIndex = 2;
+            this.quit_button.Text = "Quit";
+            this.quit_button.UseVisualStyleBackColor = true;
+            this.quit_button.Click += new System.EventHandler(this.quit_button_Click);
+            // 
+            // start_button
+            // 
+            this.start_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_button.Location = new System.Drawing.Point(279, 245);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(141, 61);
+            this.start_button.TabIndex = 1;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Palatino Linotype", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.Black;
+            this.title.Location = new System.Drawing.Point(87, -44);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(546, 350);
+            this.title.TabIndex = 0;
+            this.title.Text = "Cross The Road";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // chicken
+            // 
+            this.chicken.BackColor = System.Drawing.Color.Transparent;
+            this.chicken.Image = global::Cross_The_Road.Properties.Resources.chicken;
+            this.chicken.Location = new System.Drawing.Point(237, 473);
+            this.chicken.Name = "chicken";
+            this.chicken.Size = new System.Drawing.Size(62, 64);
+            this.chicken.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.chicken.TabIndex = 14;
+            this.chicken.TabStop = false;
+            // 
+            // car2
+            // 
+            this.car2.BackColor = System.Drawing.Color.Silver;
+            this.car2.Image = global::Cross_The_Road.Properties.Resources.green_car;
+            this.car2.Location = new System.Drawing.Point(600, 297);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(152, 95);
+            this.car2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car2.TabIndex = 13;
+            this.car2.TabStop = false;
+            // 
+            // car1
+            // 
+            this.car1.BackColor = System.Drawing.Color.Silver;
+            this.car1.Image = global::Cross_The_Road.Properties.Resources.green_car;
+            this.car1.Location = new System.Drawing.Point(-500, 134);
+            this.car1.Name = "car1";
+            this.car1.Size = new System.Drawing.Size(152, 90);
+            this.car1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car1.TabIndex = 12;
+            this.car1.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.Location = new System.Drawing.Point(454, 166);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.White;
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox7.Location = new System.Drawing.Point(339, 166);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox7.TabIndex = 10;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.White;
+            this.pictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox8.Location = new System.Drawing.Point(225, 166);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox8.TabIndex = 9;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.White;
+            this.pictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox9.Location = new System.Drawing.Point(117, 166);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackColor = System.Drawing.Color.White;
+            this.pictureBox10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox10.Location = new System.Drawing.Point(11, 166);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox10.TabIndex = 7;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(454, 336);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(339, 336);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(225, 336);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(117, 336);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 336);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 19);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Road2
+            // 
+            this.Road2.BackColor = System.Drawing.Color.Silver;
+            this.Road2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Road2.Location = new System.Drawing.Point(-7, 104);
+            this.Road2.Name = "Road2";
+            this.Road2.Size = new System.Drawing.Size(555, 147);
+            this.Road2.TabIndex = 1;
+            this.Road2.TabStop = false;
+            // 
+            // Road1
+            // 
+            this.Road1.BackColor = System.Drawing.Color.Silver;
+            this.Road1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Road1.Location = new System.Drawing.Point(-7, 269);
+            this.Road1.Name = "Road1";
+            this.Road1.Size = new System.Drawing.Size(555, 147);
+            this.Road1.TabIndex = 0;
+            this.Road1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(205)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(541, 606);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.chicken);
+            this.Controls.Add(this.car2);
+            this.Controls.Add(this.car1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Road2);
+            this.Controls.Add(this.Road1);
+            this.Name = "Form1";
+            this.Text = "Cross The Road";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chicken)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Road1;
+        private System.Windows.Forms.PictureBox Road2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox car1;
+        private System.Windows.Forms.PictureBox car2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox chicken;
+        private System.Windows.Forms.Panel menu;
+        private System.Windows.Forms.Button quit_button;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Label title;
+    }
+}
+
---
+++ Cross The Road/Cross The Road/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cross_The_Road
+{
+    public partial class Form1 : Form
+    {
+        // variabilele pentru viteza masinilor, viteza gainii si nivelul curent
+        int CAR_SPEED = 3;
+        int CHICKEN_SPEED = 15;
+        int LEVEL = 0;
+        // calea catre fisierul imagine
+        string PATH = @"Aici cheama-l pe Denis";
+
+        // functia apelata la pornirea jocului
+        public Form1()
+        {
+            InitializeComponent();
+            // initializam orientarea imaginilor masinilor
+            car1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            car2.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            // activam timerul si dezactivam butoanele si titlul
+            timer1.Enabled = true;
+
+            menu.Enabled = false;
+            menu.Visible = false;
+
+            title.Enabled = false;
+            title.Visible = false;
+
+            start_button.Enabled = false;
+            start_button.Visible = false;
+
+            quit_button.Enabled = false;
+            quit_button.Visible = false;
+
+            // pozitionam gaina si setam viteza si nivelul la valori initiale
+            chicken.Location = new Point(237, 473);
+            CAR_SPEED = 3;
+            LEVEL = 0;
+
+            menu.BackgroundImage = null;
+            // initializam imaginile masinilor
+            car1.Image = Image.FromFile(PATH + "\\green_car.png");
+            car2.Image = Image.FromFile(PATH + "\\green_car.png");
+            // rotim imaginile masinilor
+            car1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            car2.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            // trecem la urmatorul nivel
+            nextLevel();
+        }
+
+        private void quit_button_Click(object sender, EventArgs e)
+        {
+            // afisam un mesaj de confirmare si inchidem aplicatia daca se alege Yes
+            if (MessageBox.Show("Are you sure you want to quit?", "Quit Game", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // miscam masinile si verificam daca s-au intersectat cu gaina
+            car1.Left += CAR_SPEED;
+            car2.Left -= CAR_SPEED;
+            if (car1.Left > 557)
+                car1.Left = -500;
+            if (car2.Left + car2.Width < 0)
+                car2.Left = 600;
+            if (chicken.Bounds.IntersectsWith(car1.Bounds) || chicken.Bounds.IntersectsWith(car2.Bounds))
+            {
+                // daca s-a produs intersectia, jocul se incheie cu esec
+                gameOver(false);
+            }
+            // daca gaina a ajuns la marginea superioara, se trece la urmatorul nivel sau se incheie jocul cu victorie
+            if (chicken.Top + chicken.Height < 0)
+            {
+                if (LEVEL == 4)
+                    gameOver(true);
+                else
+                    nextLevel();
+            }
+
+        }
+
+        /* Functia nextLevel() creste nivelul de dificultate al jocului incrementand la final variabila SPEED. */
+        private void nextLevel()
+        {
+            chicken.Location = new Point(237, 473);
+            LEVEL++;
+            //In functie de fiecare nivel masinile isi schimb culoarea:
+            if (LEVEL == 1)
+            {
+                menu.BackColor = Color.Green;
+                menu.Visible = true;
+                title.Visible = true;
+                title.Text = "Level: 1 (easy)";
+                wait(2000);
+                title.Visible = false;
+                menu.Visible = false;
+                car1.Image = Image.FromFile(PATH + "\\green_car.png");
+                car2.Image = Image.FromFile(PATH + "\\green_car.png");
+                car1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                car2.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+            else if (LEVEL == 2)
+            {
+                menu.BackColor = Color.Blue;
+                menu.Visible = true;
+                title.Visible = true;
+                title.Text = "Level: 2 (medium)";
+                wait(2000);
+                title.Visible = false;
+                menu.Visible = false;
+                car1.Image = Image.FromFile(PATH + "\\blue_car.png");
+                car2.Image = Image.FromFile(PATH + "\\blue_car.png");
+                car1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                car2.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+            else if (LEVEL == 3)
+            {
+                menu.BackColor = Color.Purple;
+                menu.Visible = true;
+                title.Visible = true;
+                title.Text = "Level: 3 (hard)";
+                wait(2000);
+                title.Visible = false;
+                menu.Visible = false;
+                car1.Image = Image.FromFile(PATH + "\\purple_car.png");
+                car2.Image = Image.FromFile(PATH + "\\purple_car.png");
+                car1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                car2.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+            else if (LEVEL == 4)
+            {
+                menu.BackColor = Color.Red;
+                menu.Visible = true;
+                title.Visible = true;
+                title.Text = "Level: 4 (extreme)";
+                wait(2000);
+                title.Visible = false;
+                menu.Visible = false;
+                car1.Image = Image.FromFile(PATH + "\\red_car.png");
+                car2.Image = Image.FromFile(PATH + "\\red_car.png");
+                car1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                car2.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+            CAR_SPEED += 2;
+        }
+
+        //Functia gameOver() reprezinta finalul jocului. In functie de parametrul "win" determinam daca jocul a fost castigat sau pierdut. 
+        private void gameOver(bool win)
+        {
+            timer1.Enabled = false;
+
+            menu.BackgroundImage = Image.FromFile(PATH + "\\background.png");
+            menu.BackColor = Color.FromArgb(158, 205, 50);
+            menu.Enabled = true;
+            menu.Visible = true;
+
+            if (win == true)
+                title.Text = "You Win!";
+            else
+                title.Text = "Game Over!";
+            title.Enabled = true;
+            title.Visible = true;
+
+            start_button.Text = "Play Again";
+            start_button.Enabled = true;
+            start_button.Visible = true;
+
+            quit_button.Enabled = true;
+            quit_button.Visible = true;
+        }
+
+        //Functia Form1_KeyDown() modifica pozitia gainii in functie de tasta apasata.
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                chicken.Top -= CHICKEN_SPEED;
+            else if (e.KeyCode == Keys.Down)
+                chicken.Top += CHICKEN_SPEED;
+            else if (e.KeyCode == Keys.Left)
+                chicken.Left -= CHICKEN_SPEED;
+            else if (e.KeyCode == Keys.Right)
+                chicken.Left += CHICKEN_SPEED;
+        }
+
+        // Functia wait() opreste programul pentru o perioada scurta de timp specificata.
+        // https://stackoverflow.com/questions/10458118/wait-one-second-in-running-program
+        public void wait(int milliseconds)
+        {
+            this.Enabled = false;
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                this.Enabled = true;
+                timer1.Stop();
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+    }
+}
